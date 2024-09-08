1715c6d98f22298f816c1520f24b32055d82e4ec
Do Quang Tuan <quangtuan9102003@gmail.com>
2024-09-08 11:03:55 +0700

    connect to mongoDB

--- server/index.js
+++ server/index.js
@@ -2,15 +2,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true, limit: '30mb'}));
 app.use(cors());
 
+
+// Connect to MongoDB
+connectDB();
+
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
